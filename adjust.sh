2cdcf422eaaef3632097ed822851fd43de7fcbe0
Eric Boittier <boittier@master.cluster>
2021-09-21 13:13:36 +0200

    notebook for plotting convergence of GD optim.

---
+++ adjust.sh
@@ -0,0 +1,3 @@
+
+python ~/AdjustReference-System/ARS.py fbuta/4_final/refined.xyz cubes/4_SCAN_1_2_3_4_F/B.p.cube cubes/5_SCAN_1_2_3_4_F/B.p.cube ~/AdjustReference-System/fbuta/pos1/frames.txt test_4_5.mdcm.xyz
+
