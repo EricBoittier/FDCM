3d681299beeea978a25b3be437f190d8bada7c5e
Eric Boittier <boittier@master.cluster>
2021-09-22 18:30:03 +0200

    job automator

--- adjust.sh
+++ adjust.sh
@@ -1,3 +1,3 @@
 
-python ~/AdjustReference-System/ARS.py fbuta/4_final/refined.xyz cubes/4_SCAN_1_2_3_4_F/B.p.cube cubes/5_SCAN_1_2_3_4_F/B.p.cube ~/AdjustReference-System/fbuta/pos1/frames.txt test_4_5.mdcm.xyz
+python ~/AdjustReference-System/ARS.py fbuta/8_final/refined.xyz cubes/8_SCAN_1_2_3_4_F/B.p.cube cubes/9_SCAN_1_2_3_4_F/B.p.cube ~/AdjustReference-System/fbuta/pos1/frames.txt test_8_9.mdcm.xyz
 
